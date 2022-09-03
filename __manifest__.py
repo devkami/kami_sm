6b6db8ef6bd373f3d8dc72934b9c0c3253a5976a
Maicon de Menezes <maicon@kamico.com.br>
2022-09-03 19:50:04 -0300

    security hierarchy complete, but maybe has a bug on seller group rule

--- __manifest__.py
+++ __manifest__.py
@@ -7,6 +7,7 @@
   'license': 'GPL-3 or any later version',
   'depends': ['base', 'calendar', 'account', 'mail', 'rating',],
   'data': [
+    'security/security.xml',
     'security/ir.model.access.csv',
     'views/res_partner_views.xml',
     'views/res_users_views.xml',
