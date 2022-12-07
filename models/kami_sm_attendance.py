db686f87b9a46ddd20cb4fbad354011b848ccc03
Maicon de Menezes <maicon@kamico.com.br>
2022-12-07 18:06:09 -0300

    fix conflit between KAM-6-T0009 and KAM-8T00011

--- models/kami_sm_attendance.py
+++ models/kami_sm_attendance.py
@@ -13,14 +13,14 @@ class KamiInEducationAttendance(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin', 'rating.mixin']
 
     name = fields.Char(
-        string='Título', 
+        string='Título',
         required=True,
         compute='_compute_default_name',
         readonly=True
     )
     active = fields.Boolean(default=True)
     state = fields.Selection(
-        [('new', 'Aguardando Aprovação'),        
+        [('new', 'Aguardando Aprovação'),
         ('approved', 'Aprovado'),
         ('done', 'Encerrado'),
         ('waiting', 'Aguardando Cancelamento'),
@@ -30,10 +30,10 @@ class KamiInEducationAttendance(models.Model):
         tracking=True
     )
     seller_id =  fields.Many2one(
-        'res.users', 
-        string='Vendedor', 
-        default=lambda self: self.env.user         
-    )    
+        'res.users',
+        string='Vendedor',
+        default=lambda self: self.env.user
+    )
     client_id = fields.Many2one(
         'res.partner',
         string='Cliente',
@@ -54,7 +54,7 @@ class KamiInEducationAttendance(models.Model):
         copy=False,
         default=None
     )
-    expected_audience = fields.Integer(string='Público Esperado')    
+    expected_audience = fields.Integer(string='Público Esperado')
     start_date = fields.Datetime(
         string='Ínicio',
         copy=False,
@@ -64,14 +64,14 @@ class KamiInEducationAttendance(models.Model):
         string='Término',
         copy=False,
         compute='_compute_stop_date'
-    )   
+    )
     cost_ids = fields.One2many(
         'kami_sm.attendance.cost',
         'attendance_id',
-        string='Custos',        
+        string='Custos',
     )
     total_cost = fields.Monetary(
-      string='Custo Total', 
+      string='Custo Total',
       currency_field='currency_id',
       compute='_compute_attendance_total_cost'
     )
@@ -79,14 +79,11 @@ class KamiInEducationAttendance(models.Model):
       'res.currency',
       string='Currency'
     )
-    has_tasting = fields.Boolean(
-        string='Desgustação'
-    )
     description = fields.Text(string='Observações Relevantes')
     cancellation_reason = fields.Text(string='Motivo do Cancelamento')
     has_product_cost = fields.Boolean(
       'Pagamento Com Produtos',
-      default=False    
+      default=False
     )
     feedback = fields.Text(string='Comentário')
     rating = fields.Selection(
@@ -97,7 +94,7 @@ class KamiInEducationAttendance(models.Model):
         ('4', 'Muito Satisfeito'),
         ('5', 'Extremamente Satisfeito')],
         string='Nível de Satisfação',
-        default='1'     
+        default='1'
     )
     is_expired = fields.Boolean(compute='_compute_is_expired')
     address = fields.Text(string='Endereço', compute='_compute_address')
@@ -106,7 +103,7 @@ class KamiInEducationAttendance(models.Model):
     client_ids = fields.One2many(
         'kami_sm.attendance.client',
         'attendance_id',
-        string='Outros Clientes',        
+        string='Outros Clientes',
     )
     backoffice_user_id = fields.Many2one(
         'kami_sm.attendance.type',
@@ -114,28 +111,37 @@ class KamiInEducationAttendance(models.Model):
     )
 
     has_tasting = fields.Boolean(
-        string="Tem degustação" 
+        string="Tem degustação"
     )
 
     _is_beauty_day = fields.Boolean(
         compute = "_compute_is_beauty_day"
     )
-    
-    tasting_ids = fields.Many2many("kami_sm.attendance.tasting", string="Degustações")
+    tasting_ids = fields.Many2many(
+        "kami_sm.attendance.tasting",
+        string="Degustações"
+    )
+    goal_ids = fields.Many2many(
+        'kami_sm.attendance.goal',
+        string='Objetivos'
+    )
+    available_space = fields.Boolean(
+        string="O espaço do cliente comporta uma estrutura de no mínimo 1,20cm de largura?"
+    )
 
     # ------------------------------------------------------------
     # PRIVATE UTILS
     # ------------------------------------------------------------
 
     def _get_user_timezone(self):
-        return timezone(self.env.user.partner_id.tz)     
-        
+        return timezone(self.env.user.partner_id.tz)
+
     def _convert_to_user_timezone(self, date_time):
         return fields.Datetime.to_string(timezone(
             self.env.user.partner_id.tz).localize(fields.Datetime.from_string(
             date_time), is_dst=None).astimezone(utc)
         )
- 
+
     def _get_default_start_date(self):
         return self._convert_to_user_timezone( fields.Datetime.today().replace(
            hour=10, minute=00, second=00) + timedelta(days=4)
@@ -152,8 +158,8 @@ class KamiInEducationAttendance(models.Model):
             ],
             'location': attendance.address,
             'description': attendance.description
-        }            
-        self.env['calendar.event'].create(event_vals)    
+        }
+        self.env['calendar.event'].create(event_vals)
 
     def _create_attendance_invoice(self, attendance):
         for attendance_cost in attendance.cost_ids:
@@ -164,9 +170,9 @@ class KamiInEducationAttendance(models.Model):
 
                 default_partner_account = attendance.partner_id.bank_ids.\
                 search([('active', '=', True)], limit=1)
-                        
+
                 invoice_vals = {
-                    'name':f'ATENDIMENTO/EDUCAÇÃO/{attendance.id}',           
+                    'name':f'ATENDIMENTO/EDUCAÇÃO/{attendance.id}',
                     'partner_id': attendance.partner_id,
                     'create_uid': self.env.user.id,
                     'partner_bank_id': default_partner_account,
@@ -186,9 +192,9 @@ class KamiInEducationAttendance(models.Model):
                 }
                 account_move = self.env['account.move'].create(invoice_vals)
                 attendance_cost.invoice_id = account_move.id
-    
+
     def _create_attendance_rating(self, attendance):
-        rating_vals = {}       
+        rating_vals = {}
 
         if(attendance.partner_id == self.env.user.partner_id):
             rating_vals['res_model_id'] = self.env.ref('kami_sm.model_kami_sm_attendance').id
@@ -198,7 +204,7 @@ class KamiInEducationAttendance(models.Model):
             rating_vals['rating'] = attendance.rating
             rating_vals['feedback'] = attendance.feedback
             rating_vals['display_name'] = 'Educador'
-        
+
         elif(attendance.seller_id == self.env.user):
             rating_vals['res_model_id'] = self.env.ref('kami_sm.model_kami_sm_attendance').id
             rating_vals['res_id'] = self.id
@@ -207,17 +213,17 @@ class KamiInEducationAttendance(models.Model):
             rating_vals['rating'] = attendance.rating
             rating_vals['feedback'] = attendance.feedback
             rating_vals['Vendedor'] = 'Vendedor'
-        
+
         self.env['rating.rating'].create(rating_vals)
-    
+
     def _create_attendance_client(self, attendance):
         client_vals = {}
         client_vals['attendance_id'] = attendance.id
         client_vals['partner_id'] = attendance.client_id
         client_vals['served_audience'] = attendance.served_audience
-                
+
         self.env['rating.rating'].create(client_vals)
-    
+
     # ------------------------------------------------------------
     # CONSTRAINS
     # ------------------------------------------------------------
@@ -227,7 +233,7 @@ class KamiInEducationAttendance(models.Model):
         for attendance in self:
             minimum_antecedence = fields.Datetime.today() + timedelta(days=4)
             if(attendance.start_date < minimum_antecedence):
-                raise ValidationError(' A antecedência mínima para o agendamento de um evento são 4 dias!')        
+                raise ValidationError(' A antecedência mínima para o agendamento de um evento são 4 dias!')
 
     # ------------------------------------------------------------
     # COMPUTES
@@ -249,18 +255,18 @@ class KamiInEducationAttendance(models.Model):
     @api.depends('type_id', 'theme_id', 'partner_id')
     def _compute_default_name(self):
       for attendance in self:
-        attendance.name = f'{attendance.type_id.name}-{attendance.theme_id.name}'    
-    
+        attendance.name = f'{attendance.type_id.name}-{attendance.theme_id.name}'
+
     @api.depends('start_date')
     def _compute_stop_date(self):
         for attendance in self:
             attendance.stop_date = attendance.start_date + timedelta(hours=8)
-    
+
     def _compute_is_expired(self):
         for attendance in self:
             attendance.is_expired = attendance.state == 'approved'\
             and attendance.start_date < fields.Datetime.now()
-        
+
     def _compute_address(self):
         for attendance in self:
             attendance.address = f'{attendance.client_id.street} - {attendance.client_id.street2}, {attendance.client_id.city}, {attendance.client_id.state_id.name}, CEP: {attendance.client_id.zip}'
@@ -274,9 +280,9 @@ class KamiInEducationAttendance(models.Model):
             if attendance.state != 'new':
                 raise UserError('Somente Novos Atendimentos Podem Ser Aprovados!')
             else:
-                attendance.state = 'approved'                
+                attendance.state = 'approved'
                 self._create_attendance_invoice(attendance)
-                self._create_attendance_event(attendance)            
+                self._create_attendance_event(attendance)
 
     def action_request_cancel(self):
         for attendance in self:
@@ -284,8 +290,8 @@ class KamiInEducationAttendance(models.Model):
                 raise UserError('Somente Atendimentos Novos ou Aprovados Podem ser Cancelados!')
             else:
                 attendance.state = 'waiting'
-    
-    def action_open_request_cancel(self):        
+
+    def action_open_request_cancel(self):
         return {
             'res_model': 'kami_sm.attendance',
             'res_id': self.id,
@@ -294,7 +300,7 @@ class KamiInEducationAttendance(models.Model):
             'view_id': self.env.ref('kami_sm.kami_sm_attendance_view_popup_form').id,
             'target': 'new'
         }
-    
+
     def action_cancel_popup_attendance(self):
         return {'type': 'ir.actions.act_window_close'}
 
@@ -306,7 +312,7 @@ class KamiInEducationAttendance(models.Model):
                 attendance.state = 'canceled'
 
     def action_rating_attendance(self):
-        for attendance in self:   
+        for attendance in self:
             if attendance.state not in ['approved', 'done']:
                 raise UserError('Somente Atendimentos Aprovados Podem ser Avaliados/Encerrados!')
             else:
@@ -320,13 +326,13 @@ class KamiInEducationAttendance(models.Model):
                     'view_id': self.env.ref('kami_sm.kami_sm_attendance_rating_view_popup_form').id,
                     'target': 'new'
                 }
-                    
+
     def action_get_ratings(self):
         action = self.env['ir.actions.act_window']._for_xml_id('rating.rating_rating_view')
         return dict(
             action,
             domain=[('res_id', 'in', self.ids), ('res_model', '=', 'kami_sm.attendance')],
-        )    
+        )
 
     def action_rating_or_finish(self):
         for attendance in self:
@@ -336,11 +342,11 @@ class KamiInEducationAttendance(models.Model):
                 attendance.rating_send_request(rating_template, force_send=True)
                 #registra avaliação do educador
                 self._create_attendance_rating(attendance)
-                attendance.state = 'done'                
+                attendance.state = 'done'
             elif(attendance.seller_id == self.env.user):
                 #registra avaliação do vendedor
-                self._create_attendance_rating(attendance)    
-    
+                self._create_attendance_rating(attendance)
+
     # ------------------------------------------------------------
     # PARTNERS DOMAIN FILTERS
     # ------------------------------------------------------------
@@ -351,9 +357,8 @@ class KamiInEducationAttendance(models.Model):
             partner_attendances = []
             partner_types = attendance.type_id.partner_ids.mapped('id')
             partner_themes = attendance.theme_id.partner_ids.mapped('id')
-            seller_partners = self.env.user.partner_ids.mapped('id')
-            attendances = self.env['kami_sm.attendance'].search([                
-                ('start_date', '=', attendance.start_date)                
+            attendances = self.env['kami_sm.attendance'].search([
+                ('start_date', '=', attendance.start_date)
             ])
             for att in attendances:
                 if(att.partner_id.id):
@@ -365,20 +370,19 @@ class KamiInEducationAttendance(models.Model):
                 ('allday', '=', True),
                 ('start_date', '=', attendance.start_date)
             ]).partner_ids.mapped('id')
-          
+
             return {'domain':
                 {'partner_id':
                 [   ('id', 'in', partner_types),
                     ('id', 'in', partner_themes),
-                    ('id', 'in', seller_partners),
                     ('id', 'not in', partner_meetings),
                     ('id', 'not in', partner_attendances)
                 ]}}
-    
+
     # ------------------------------------------------------------
     # RATING MIXIN
     # ------------------------------------------------------------
-    
+
     def rating_get_partner_id(self):
         if self.client_id:
             return self.client_id
