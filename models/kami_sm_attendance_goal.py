75f01201229281e5dc6f8b36cebb3ee9ca9d7d8b
Maicon de Menezes <maicon@kamico.com.br>
2022-12-07 16:38:25 -0300

    Revert "model attendance goal created"
    This reverts commit 8640c0ddc5741bd791fc82530594da1f6ab1e7b6.

--- models/kami_sm_attendance_goal.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-class KamiSmAttendanceGoal(models.Model):
-    _name = "kami_sm.attendance.goal"
-    _description = "Kami Model For Attendance Goal"
-
-    name = fields.Char("Nome")
-    description = fields.Text("Descrição")
-    active = fields.Boolean()
