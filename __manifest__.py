5227be66b3af62e580791eabbcab9806714cee38
Maicon de Menezes <maicon@kamico.com.br>
2023-01-25 09:53:34 -0300

    add generate tasks option to kami_sm.attendance.type

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,7 @@
   'summary': 'Kamico Service Manager',
   'version': '1.0',
   'license': 'GPL-3 or any later version',
-  'depends': ['base', 'calendar', 'account', 'mail', 'rating',],
+  'depends': ['base', 'calendar', 'account', 'mail', 'rating', 'project'],
   'data': [
     'security/security.xml',
     'security/ir.model.access.csv',
